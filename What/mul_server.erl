3f7c35e194d95adcd327c18f09f31fee8d99dd80
Bar Leshem <barlesh8@gmail.com>
2015-08-16 00:35:04 +0300

    v2

--- What/mul_server.erl
+++ What/mul_server.erl
@@ -6,16 +6,14 @@
 
 -compile(export_all).
 -include_lib("wx/include/wx.hrl").
--define(max_x,(1000)).
--define(max_y,(1000)).
 -define(REFRESH_TIME, 80).
 -define(STEP, 10).
 -define(ABOUT,?wxID_ABOUT).
 -define(EXIT,?wxID_EXIT).
 
--record(state, {frame, panel, nw_img, ww_img, z_img, corpse_img, bg, database}). 
+-record(state, {frame, panel, nw_img, ww_img, z_img, corpse_img, sparks_img, tree1, tree2, tree3, tree4, tree5, bg, database}). 
   
-run()-> spawn(fun()->start() end).
+run(Size)-> spawn(fun()->start(Size) end).
   %% Creeates the window and menus etc.
 
 setFrame(Frame) ->
@@ -31,10 +29,10 @@ setFrame(Frame) ->
   	wxFrame:setStatusText(Frame,"Welcome to wxErlang"),
   	wxFrame:connect(Frame, command_menu_selected),
   	wxFrame:connect(Frame, close_window).
-start()->
+start(BOARD_SIZE)->
 	register(multimedia_server, self()),
 	Wx = wx:new(),
-	Frame = wxFrame:new(Wx, -1, "GOT SIM", [{size, {?max_x, ?max_y}}]),    
+	Frame = wxFrame:new(Wx, -1, "GOT SIM", [{size, {BOARD_SIZE, BOARD_SIZE}}]),    
 	setFrame(Frame),
 	Panel = wxPanel:new(Frame),
 	wxFrame:connect(Panel, paint),
@@ -42,6 +40,12 @@ start()->
 	Image2 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/ww.png"),
 	Image3 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/z.png"),
 	Image4 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/snir/car.png"),
+	Image5 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/tree1.png"),
+	Image6 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/tree2.png"),
+	Image7 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/tree3.png"),
+	Image8 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/tree4.png"),
+	Image9 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/tree5.png"),
+	Image10 = wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/sparks.png"),
 	BG=wxBitmap:new(wxImage:new("/home/barlesh/Erworkspace/FinalProject/bar/snow-bg.png")),
 	wxFrame:show(Frame),
 	ClientDC = wxClientDC:new(Panel),
@@ -53,7 +57,7 @@ start()->
 	%temp%
 	register(refresh_Process, spawn(fun()->refreshSim() end) ),
 	State = #state{frame=Frame, panel=Panel, nw_img=Image, ww_img=Image2, 
-		z_img=Image3, corpse_img=Image4, bg=BG, database=ETS_table},
+		z_img=Image3, corpse_img=Image4, sparks_img = Image10, tree1 = Image5, tree2 = Image6,tree3 = Image7,tree4 = Image8, tree5 = Image9, bg=BG, database=ETS_table},
 	loop(State).
 
 eval(From,From) -> From;
@@ -61,6 +65,12 @@ eval(From, To) ->
 	if From<To -> erlang:min(From+?STEP, To);
 	true-> erlang:max(From-?STEP, To) end.
 		
+drewFight(Location) ->
+	multimedia_server!{init, {sparks, self(), Location}},
+	receive
+	after ?REFRESH_TIME * 6 -> multimedia_server!{remove, {self()}}
+	end.
+
 moveCharacter(_,_,L,L) -> exit(-1);
 %this function get a character's type, name and movemnet (from and to). it spawns a new process to sent main process new locations update every refresh time [ms] 
 moveCharacter(Type, Name,{X_Curr, Y_Curr}, {X_Dest, Y_Dest} )-> 
@@ -71,32 +81,38 @@ moveCharacter(Type, Name,{X_Curr, Y_Curr}, {X_Dest, Y_Dest} )->
 				%if (X-X_Curr =:= 0) -> Angle = 0.5*math:pi();
 				%true -> Angle = math:tan(Y-Y_Curr, X-X_Curr ) * (mathi:pi()/180) end, 
 				Angle =0,
-				multimedia_server! {update, {Type, Name, {X,Y}, Angle } },
+				multimedia_server! {update, self(), {Type, Name, {X,Y}, Angle } },
 				moveCharacter(Type, Name, {X,Y}, {X_Dest, Y_Dest} )
 	end.
 %this function updates character's location at 	ETS
-updateETS(ETS_table, Type, Name, New_Location, Angle )->
-	ets:insert(ETS_table,{Name, {Type,New_Location, Angle} } ).
+updateETS(ETS_table, Type, Name, New_Location, Angle, Updating_Pid )->
+	ets:insert(ETS_table,{Name, {Type,New_Location, Angle, Updating_Pid} } ).
 	%temp
 	%io:format("new location is ~p~n", [ets:lookup(ETS_table, Name)]).
 
 %no more items to show
-show(_,_, '$end_of_table',_,_,_,_) -> done;
+show(_,_, '$end_of_table',_,_,_,_,_,_,_,_,_,_) -> done;
 
 %this function get Key of next item to show at screen
-show(DC,ETS_table, Key, NW_IMG, WW_IMG, Z_IMG, CORPSE_IMG) -> 
-	[{Key, {Type, Pos, Angle}}] = ets:lookup(ETS_table, Key),
+show(DC,ETS_table, Key, NW_IMG, WW_IMG, Z_IMG, CORPSE_IMG, SPARK_IMG, TREE1, TREE2, TREE3, TREE4, TREE5) -> 
+	[{Key, {Type, Pos, Angle,_}}] = ets:lookup(ETS_table, Key),
 	case Type of 
 		warrior -> Temp =  wxImage:rotate(NW_IMG,Angle, Pos);
 		white_walker -> Temp =  wxImage:rotate(WW_IMG,Angle, Pos);
 		zombie -> Temp =  wxImage:rotate(Z_IMG,Angle, Pos);
-		body -> Temp =  wxImage:rotate(CORPSE_IMG,Angle, Pos)
+		body -> Temp =  wxImage:rotate(CORPSE_IMG,Angle, Pos);
+		sparks -> Temp = wxImage:rotate(SPARK_IMG,Angle, Pos);
+		tree1 ->  Temp=xwImage:rotate(TREE1, Angle , Pos);
+		tree2 -> Temp = xwImage:rotate(TREE2, Angle , Pos);
+		tree3 -> Temp = xwImage:rotate(TREE3, Angle , Pos);
+		tree4 -> Temp = xwImage:rotate(TREE4, Angle , Pos);
+		tree5 -> Temp = xwImage:rotate(TREE5, Angle , Pos)
 	end,
 	Bitmap=wxBitmap:new(Temp),
 	wxDC:drawBitmap(DC, Bitmap, Pos),
 	wxImage:destroy(Temp),
 	wxBitmap:destroy(Bitmap),
-	show(DC,ETS_table, ets:next(ETS_table, Key), NW_IMG, WW_IMG, Z_IMG, CORPSE_IMG).
+	show(DC,ETS_table, ets:next(ETS_table, Key), NW_IMG, WW_IMG, Z_IMG, CORPSE_IMG,SPARK_IMG, TREE1, TREE2, TREE3, TREE4, TREE5).
 	
 %this function refresh screen accoring to ETS 
 showSim(State) ->
@@ -104,7 +120,7 @@ showSim(State) ->
 	DC=wxBufferedDC:new(ClientDC),
 	wxDC:drawBitmap(DC, State#state.bg, {0,0}),
 	
-	show(DC,State#state.database, ets:first(State#state.database), State#state.nw_img, State#state.ww_img, State#state.z_img, State#state.corpse_img),
+	show(DC,State#state.database, ets:first(State#state.database), State#state.nw_img, State#state.ww_img,State#state.z_img,State#state.corpse_img,State#state.sparks_img,State#state.tree1,State#state.tree2,State#state.tree3,State#state.tree4,State#state.tree5),
 	wxBufferedDC:destroy(DC),
 	wxClientDC:destroy(ClientDC).
 
@@ -114,6 +130,20 @@ refreshSim() ->
 	stop -> exit(-1)
 	after ?REFRESH_TIME -> multimedia_server!show
 	end, refreshSim().
+
+
+showResult(Frame, NW, WW, Z) ->
+	io:format("Simulation ended!!!!!!!!!!!!!!!!!!!!!!~n~n"),
+	if NW =:= 0 -> Text = "Man have lost the buttle";
+	true -> Text = "Victory!!!! We are all saved" end,
+	D = wxMessageDialog:new (Frame, Text),
+	wxMessageDialog:showModal (D). 
+
+%verify that sending process is relevent to the Name it is trying to update.
+varify(_,_, []) -> 0;
+varify(Pid, _,  [{ _, {_,_,_,Pid} }] ) -> 1;
+varify(_, Type, [{_, {Type,_,_,_} }] ) -> 1;
+varify(_,_,_)-> 0.
 
 %routine happanes every time multimedia server closed
 close_mull_server() ->
@@ -129,13 +159,17 @@ close_mull_server() ->
 %f. (user) statistics window TODO
 loop(State)->
 	receive
-	{init, {Type, Name, Location} } -> updateETS(State#state.database, Type, Name, Location, 0 );
-	{update, {Type, Name,  New_Location, Angle} } -> L = ets:lookup(State#state.database, Name),
-					if length(L) =:= 0 -> ignored;
-					true -> updateETS(State#state.database, Type, Name, New_Location, Angle ) end; 
+	{init, {Type, Name, Location} } -> updateETS(State#state.database, Type, Name, Location, 0 , none);
+	{update, Pid, {Type, Name,  New_Location, Angle} } -> Ans = varify(Pid,Type, ets:lookup(State#state.database, Name)),
+					case Ans of 
+					0 -> ignore;
+					1-> updateETS(State#state.database, Type, Name, New_Location, Angle, Pid ) 
+					end;
 	{remove, {Name}}-> ets:delete(State#state.database, Name);
-	{A,B,C,D} -> Text="Warrios:"++integer_to_list(A)++"			Zombies:"++integer_to_list(B)++"			White Walkers:"++integer_to_list(C)++"			resorections"++integer_to_list(D),
-	wxFrame:setStatusText(State#state.frame,Text);
+	{sim_ended, NW, WW, Z} -> showResult(State#state.frame, NW, WW, Z);
+	{status_text,A,B,C,D} -> 	io:format("at mul_server:loop A:~p,B:~p, C:~p, D:~p~n", [A,B,C,D]),
+			Text="Warrios:"++integer_to_list(A)++"			White Walkers:"++integer_to_list(B)++"			Zombies:"++integer_to_list(C)++"			resorections"++integer_to_list(D),
+			wxFrame:setStatusText(State#state.frame,Text);
 	show -> showSim(State);
 	stop -> close_mull_server();
 	%user interface msg
--- What/server_gate.erl
+++ What/server_gate.erl
@@ -2,15 +2,43 @@
 -compile(export_all).
 -author('BarLesh').
 
+-record(state, {all_created}).
+
 start() ->
 	register(server_gate, self() ),
-	loop().
-loop()->
+	loop(#state{all_created = 0}).
+
+loop(State)->
 	receive
+	done when State#state.all_created =:= 3 -> loop2();
+	done -> X = State#state.all_created,New_State=State#state{all_created=X+1};
+	{stats_update, NW_C,WW_C,Z_C,RES} -> multimedia_server!{status_text,NW_C,WW_C,Z_C,RES}
+	after 0 -> server()
+	end, loop(State).
+
+loop2() -> 
+	receive
+	{stats_update, NW_C,WW_C,Z_C,RES} ->
+			if ((NW_C =:= 0) orelse (WW_C+Z_C =:= 0))-> ignore;%TODO!!!!!!!
+			true -> multimedia_server!{status_text,NW_C,WW_C,Z_C,RES} end
+	after 0 -> server() end, loop2().
+	
+server()->
+	receive
+	{born, {trees, Name, Location} } -> X = random:uniform(5), case X of
+									1-> Type = tree1;
+									2-> Type = tree2;
+									3-> Type = tree3;
+									4-> Type = tree4;
+									5-> Type = tree5
+									end,
+					io:format("at server_gate tree name is ~p~n", [Name]),
+					multimedia_server!{init, {Type, Name,  Location} };
 	{born, {Type, Name, Location} } -> multimedia_server!{init, {Type, Name,  Location} },
 					statistics_server!{Type, created};
-	{death, {Type, Name}} -> multimedia_server!{remove, {Name} },
-			statistics_server!{Type, killed};
+	{death, {Type, Name}} -> io:format("at server_gate, ~p killed~n",[Name]),
+				multimedia_server!{remove, {Name} },
+				statistics_server!{Type, killed};
 	{transition, {Original_Type, Trans_Type, Name, Location} } -> multimedia_server!{remove, {Name} },
 						multimedia_server!{init, {Trans_Type, Name,  Location} }, 
 								case Original_Type of 
@@ -20,9 +48,7 @@ loop()->
 								end;
 	{movement, {Type, Name, {X1,Y1}, {X2,Y2} }  } -> spawn(fun()-> mul_server:moveCharacter(Type, Name, 
 									{trunc(X1), trunc(Y1)}, {trunc(X2), trunc(Y2)} ) end);	
-	{fight, {{Type1, Name1}, {Type2, Name2}, Location}} -> [];%TODO
-	{won_figth, {Type, Name, New_Location}} -> []; %updateETS(State#state.database, Type, Name, New_Location, 0 )
+	{fight, {{Type1, Name1}, {Type2, Name2}, Location}} -> mul_server:drewFight(Location);
+	{won_figth, {Type, Name, New_Location}} ->[]; %updateETS(State#state.database, Type, Name, New_Location, 0 )
 	stop -> exit(-1)	
-
-
-	end, loop().
+	end.
